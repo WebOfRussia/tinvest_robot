85a9f343777a4311da215caafb7fba5116a46bf0
Aleksandr Perevalov <aleksandr.perevalov@hs-anhalt.de>
2022-05-22 19:12:45 +0200

    upd requirements

--- tests/test_sentiment_analyzer.py
+++
@@ -1,6 +0,0 @@
-from tinvest_robot_perevalov.news_fetcher import _init_sentiment_analyzer
-    
-def test_init_sentiment_analyzer():
-    sentiment_analyzer = _init_sentiment_analyzer()
-    assert 'negative' == sentiment_analyzer.predict_sentiment("Very bad news")
-    assert 'positive' == sentiment_analyzer.predict_sentiment("The best news ever")
